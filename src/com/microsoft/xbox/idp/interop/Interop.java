0bd37e963010fe4c1812a8d2b56f887fb8e99bcc
derderder <derderder77380@gmail.com>
2016-08-14 18:40:03 +0200

    Add dummy Interop class

---
+++ src/com/microsoft/xbox/idp/interop/Interop.java
@@ -0,0 +1,19 @@
+package com.microsoft.xbox.idp.interop;
+
+import android.content.Context;
+import android.util.Log;
+
+public class Interop {
+	
+	public Interop() {}
+
+	public static String ReadConfigFile(Context context) {
+		Log.i("GenericLauncher","Interop: ReadConfigFile");
+		return "";
+	}
+
+	public static String getSystemProxy() {
+		Log.i("GenericLauncher","Interop: getSystemproxy");
+		return "";
+	}
+}
